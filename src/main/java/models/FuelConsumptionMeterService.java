6991fb399b974b9b69e0bb788c1f6c9750068aab
Donimika <141586347+Donimika@users.noreply.github.com>
2024-04-09 19:07:27 +0200

    calculate method in service added

--- src/main/java/models/FuelConsumptionMeterService.java
+++ src/main/java/models/FuelConsumptionMeterService.java
@@ -1,7 +1,13 @@
 package models;
 
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class FuelConsumptionMeterService {
+    public float calculate(FuelConsumptionMeterDTO fuelConsumptionMeterDTO) {
+        float totalFuelNeeded = fuelConsumptionMeterDTO.getNumberOfKilometers() * (fuelConsumptionMeterDTO.getAverageConsumption() / 100);
+        float totalCostOfFuel = totalFuelNeeded * fuelConsumptionMeterDTO.getFuelPrice();
+        return totalCostOfFuel;
+    }
 }
