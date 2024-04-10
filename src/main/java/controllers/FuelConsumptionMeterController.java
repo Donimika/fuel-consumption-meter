990380349c4ddfcfb01cb8d53bd53f7084e0c774
Donimika <141586347+Donimika@users.noreply.github.com>
2024-04-10 10:59:28 +0200

    calculate method in controller added

--- src/main/java/controllers/FuelConsumptionMeterController.java
+++ src/main/java/controllers/FuelConsumptionMeterController.java
@@ -1,17 +1,29 @@
 package controllers;
 
 import models.FuelConsumptionMeterDTO;
+import models.FuelConsumptionMeterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("app")
 public class FuelConsumptionMeterController {
+    @Autowired
+    FuelConsumptionMeterService fuelConsumptionMeterService;
 
     @GetMapping
     public String renderCounter(@ModelAttribute FuelConsumptionMeterDTO fuelConsumptionMeterDTO) {
+        return "fuel-consumption-meter";
+    }
+
+    @PostMapping
+    public String calculateTotalCostOfFuel(@ModelAttribute FuelConsumptionMeterDTO fuelConsumptionMeterDTO, Model model) {
+        model.addAttribute("totalCostOfFuel", fuelConsumptionMeterService.calculateTotalCostOfFuel(fuelConsumptionMeterDTO));
         return "fuel-consumption-meter";
     }
 }
--- src/main/java/models/FuelConsumptionMeterService.java
+++ src/main/java/models/FuelConsumptionMeterService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FuelConsumptionMeterService {
-    public float calculate(FuelConsumptionMeterDTO fuelConsumptionMeterDTO) {
+    public float calculateTotalCostOfFuel(FuelConsumptionMeterDTO fuelConsumptionMeterDTO) {
         float totalFuelNeeded = fuelConsumptionMeterDTO.getNumberOfKilometers() * (fuelConsumptionMeterDTO.getAverageConsumption() / 100);
         float totalCostOfFuel = totalFuelNeeded * fuelConsumptionMeterDTO.getFuelPrice();
         return totalCostOfFuel;
