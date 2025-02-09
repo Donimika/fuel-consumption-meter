e818cc15769109377143cb12a203dd05d3da159b
Dominika Donátková <dominika.donatkova@o2its.cz>
2025-02-09 21:10:12 +0100

    config + translation

---
+++ src/main/java/cz/app/config/WebConfig.java
@@ -0,0 +1,38 @@
+package cz.app.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.forLanguageTag("en"));
+        return localeResolver;
+    }
+}
--- src/main/java/cz/app/controllers/FuelConsumptionMeterController.java
+++ src/main/java/cz/app/controllers/FuelConsumptionMeterController.java
@@ -6,10 +6,7 @@ import cz.app.models.dtos.TotalKilometersDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/counter")
@@ -18,19 +15,26 @@ public class FuelConsumptionMeterController {
     FuelConsumptionMeterService fuelConsumptionMeterService;
 
     @GetMapping
-    public String renderFuelConsumptionMeter(@ModelAttribute TotalCostDTO totalCostDTO, @ModelAttribute TotalKilometersDTO totalKilometersDTO) {
+    public String renderFuelConsumptionMeter(@ModelAttribute TotalCostDTO totalCostDTO, @ModelAttribute TotalKilometersDTO totalKilometersDTO,
+                                             @RequestParam(value = "lang", defaultValue = "en") String lang,
+                                             Model model) {
+        model.addAttribute("lang", lang);
         return "fuel-consumption-meter";
     }
 
     @PostMapping("/totalcost")
-    public String calculateTotalCostOfFuel(@ModelAttribute TotalCostDTO totalCostDTO, Model model, TotalKilometersDTO totalKilometersDTO) {
+    public String calculateTotalCostOfFuel(@ModelAttribute TotalCostDTO totalCostDTO, Model model, TotalKilometersDTO totalKilometersDTO,
+                                           @RequestParam(value = "lang", defaultValue = "en") String lang) {
         model.addAttribute("totalCost", fuelConsumptionMeterService.calculateTotalCostOrKilometers(totalCostDTO, true));
+        model.addAttribute("lang", lang);
         return "fuel-consumption-meter";
     }
 
     @PostMapping("/totalkilometers")
-    public String calculateTotalKilometers(@ModelAttribute TotalKilometersDTO totalKilometersDTO, Model model, TotalCostDTO totalCostDTO) {
+    public String calculateTotalKilometers(@ModelAttribute TotalKilometersDTO totalKilometersDTO, Model model, TotalCostDTO totalCostDTO,
+                                           @RequestParam(value = "lang", defaultValue = "en") String lang) {
         model.addAttribute("totalKilometers", fuelConsumptionMeterService.calculateTotalCostOrKilometers(totalKilometersDTO, false));
+        model.addAttribute("lang", lang);
         return "fuel-consumption-meter";
     }
 }
